93e1f0ac151f1cc304ad4a99ee0182c7f2d6f642
4xlifepro-sys <4xlifepro@gmail.com>
2026-09-13 16:16:17 +0300

    fix: import useRef in admin page

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import { 
